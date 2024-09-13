c23351561d0e7bdc1890fffe5945e90259f1d693
Gigvo <giganiusrevo@gmail.com>
2024-09-13 15:20:10 +0700

    Add files via upload

---
+++ Pertemuan 3/Soal 1/PersamaanKuadrat.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double a,b,c,D;
+	cout<<"Input coefficient a: "<<"\n";
+	cin>>a;
+	cout<<"Input coefficient b: "<<"\n";
+	cin>>b;
+	cout<<"Input coefficient c: "<<"\n";
+	cin>>c;
+	
+	D = pow(b,2.0) - (4*a*c);
+	cout<<"Nilai Determinan: "<<D<<"\n";
+	if (D>0){
+		double x1, x2;
+	 	x1 = ((b*(-1)) + sqrt(D)) / 2*a;
+	 	x2 = ((b*(-1)) - sqrt(D)) / 2*a;
+	 	cout << "Akar pertama: " << x1 << "\n";
+	 	cout << "Akar kedua: " << x2 << "\n";
+	} else if (D==0){
+		double x;
+	 	x = (-b) / (2*a);
+	 	cout << "Hasil akar: "<< x << "\n";
+	} else if (D<0){
+	 	cout << "Tidak ada akar nyata (akarnya kompleks)";
+	}
+	 	
+}
