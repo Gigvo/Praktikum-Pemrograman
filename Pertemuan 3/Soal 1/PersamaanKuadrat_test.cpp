052dd93312112b88f7edb701dfa0b78cd1f4cd42
Gigvo <giganiusrevo@gmail.com>
2024-11-04 20:43:17 +0700

    Add files via upload

---
+++ Pertemuan 3/Soal 1/PersamaanKuadrat_test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+int main() {
+	double a,b,c,D;
+	ifstream infile;
+    infile.open("testcase.txt");
+	while(infile.is_open()){
+        infile >> a >> b >> c;
+        if (a == 0) {
+            break;
+            infile.close();
+        }
+        cout << "Input a = " << a << " Input b = " << b << " Input c = " << c << endl;
+        D = pow(b,2.0) - (4*a*c);
+	    cout<<"Nilai Determinan: "<<D<<"\n";
+	if (D>0){
+		double x1, x2;
+	 	x1 = ((b*(-1)) + sqrt(D)) / (2*a);
+	 	x2 = ((b*(-1)) - sqrt(D)) / (2*a);
+	 	cout << "Akar pertama: " << x1 << "\n";
+	 	cout << "Akar kedua: " << x2 << "\n";
+	} else if (D==0){
+		double x;
+	 	x = (-b) / (2*a);
+	 	cout << "Hasil akar: "<< x << "\n";
+	} else if (D<0){
+	 	cout << "Tidak ada akar nyata (akarnya kompleks)";
+	}
+    }
+	
+	 	
+}
